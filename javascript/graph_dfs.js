74594bfd6f08b05bcd4b828ca8d9a062d95fe328
RomanLatipov <romanlatipov@gmail.com>
2024-07-22 22:00:32 -0400

    complete graph-dfs lab

--- javascript/graph_dfs.js
+++ javascript/graph_dfs.js
@@ -1,24 +1,58 @@
 function isPath(graph, vertexA, vertexB) {
-  // type your code here
+  for(const elem in graph){
+    if(graph[elem].includes(vertexB))
+      return recursiveSearch(graph, vertexA, vertexB, vertexA)
+  }
+  
+  return false;
+}
+function recursiveSearch(graph, vertexA, vertexB, start) {
+  if (graph[vertexA].length === 0) return false;
+  if (graph[vertexA].includes(vertexB)) return true;
+
+  for(const elem of graph[vertexA]) {
+    // console.log(graph[vertexA])
+    if (elem !== start) {
+      console.log(elem)
+      if(recursiveSearch(graph, elem, vertexB, start))
+        return true;
+    }
+  }
+  return false;
 }
 
 if (require.main === module) {
   // add your own tests in here
-  let graph = {
-    jan: ["john", "jambaby"],
+  // let graph = {
+  //   jan: ["john", "jambaby"],
+  //   john: ["carl"],
+  //   jambaby: [],
+  //   carl: ["jambaby"],
+  //   dave: []
+  // };
+
+  // console.log("Expecting: true");
+  // console.log(isPath(graph, "jan", "carl"));
+
+  // console.log("");
+
+  // console.log("Expecting: false");
+  // console.log(isPath(graph, "jan", "dave"));
+  const complexGraph = {
+    jan: ["john", "jambaby", "malala"],
     john: ["carl"],
     jambaby: [],
-    carl: ["jambaby"],
-    dave: []
+    carl: ["jambaby", "dave", "martin"],
+    dave: ["jan"],
+    mittens: [],
+    martin: ["mittens"],
+    malala: ["dave", "carl", "martin", "pirate"],
+    pirate: ["shiba", "inu"],
+    shiba: [],
+    inu: []
   };
 
-  console.log("Expecting: true");
-  console.log(isPath(graph, "jan", "carl"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log(isPath(graph, "jan", "dave"));
+  console.log(isPath(complexGraph, "carl", "inu"))
 }
 
 module.exports = isPath;
